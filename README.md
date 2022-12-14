dfa3cb8e2905f2dacc79fe7e2552b6eb2616b2ed
auranebes <ars352@yahoo.com>
2022-12-14 18:08:59 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CardValidationTF
+
+A description of this package.
---
+++ Sources/CardValidationTF/CardValidationTF.swift
@@ -0,0 +1,6 @@
+public struct CardValidationTF {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
